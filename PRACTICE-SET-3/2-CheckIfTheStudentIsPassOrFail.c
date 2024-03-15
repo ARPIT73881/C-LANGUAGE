e28d2ee05cd1e153c3243846c6bc3f920a41cb80
ARPIT SHARMA <AS.73881@GMAIL.COM>
2024-03-15 22:29:11 +0530

    Program to check if the student passes the test or not .

--- PRACTICE-SET-3/2-CheckIfTheStudentIsPassOrFail.c
+++ PRACTICE-SET-3/2-CheckIfTheStudentIsPassOrFail.c
@@ -1,4 +1,4 @@
-// Write a program to find whether a student is pass or fail , if it requires total 40% marks and atleast 33% marks in each subject ( subjects - physics , chemistry , maths)
+// Write a program to find whether a student is pass or fail , if it requires total 40% marks and atleast 33% marks in each subject ( assume the subjects are  - physics , chemistry , maths)
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
   scanf("%d", &chemistry);
   printf("Enter the marks in maths = ");
   scanf("%d", &maths);
-  average = (physics + chemistry + maths) / 3;
+  average = (physics + chemistry + maths) / 3.0;
   if (average < 40 || physics < 33 || chemistry < 33 || maths < 33)
   {
     printf("The student's total percentage is %0.2f %% and he fails the exam", average);
