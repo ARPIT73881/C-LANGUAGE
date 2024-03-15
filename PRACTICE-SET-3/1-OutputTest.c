4fe73b26e2532222a39d6698d1df1d104cd3f3b8
ARPIT SHARMA <AS.73881@GMAIL.COM>
2024-03-15 22:22:29 +0530

    Program to check the output of the question

---
+++ PRACTICE-SET-3/1-OutputTest.c
@@ -0,0 +1,19 @@
+// What will be the output of the program
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int a = 10;
+  if (a = 11)
+  {
+    printf("I am 11\n");
+  }
+  else
+  {
+    printf("I am not 11\n");
+  }
+  printf("The value of a is %d", a);
+
+  // answer is 11 , result will be i am  11
+  return 0;
+}
